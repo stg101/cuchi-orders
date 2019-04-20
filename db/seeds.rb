647f3f22bb9966abbece67a4f3dffe92deb40472
Santiago <ry.yrupailla@gmail.com>
2019-04-19 23:23:28 -0500

    Add emoji to products

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,8 +7,8 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 Product.delete_all
-Product.create( name: 'palta', price: 15, available: true)
-Product.create( name: 'queso', price: 15, available: true)
-Product.create( name: 'huevo con hotdog', price: 15, available: true)
-Product.create( name: 'torreja', price: 15, available: true)
-Product.create( name: 'pollo', price: 20, available: true)
+Product.create( name: 'palta', price: 15, available: true, emoji: '🥑')
+Product.create( name: 'queso', price: 15, available: true, emoji: '🧀')
+Product.create( name: 'huevo con hotdog', price: 15, available: true, emoji: '🌭')
+Product.create( name: 'torreja', price: 15, available: true, emoji: '🍳')
+Product.create( name: 'pollo', price: 20, available: true, emoji: '🍗')
