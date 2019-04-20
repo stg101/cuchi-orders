0b884b48537ca2cd7e05c9087c4f301e9610c7d4
Santiago <ry.yrupailla@gmail.com>
2019-04-20 18:32:30 -0500

    Fix error in user callback

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -3,6 +3,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     @user = User.from_omniauth(request.env["omniauth.auth"])
 
     if @user
+      sign_in_and_redirect @user
       set_flash_message(:notice, :success, kind: "Slack")
     else
       session["devise.slack_data"] = request.env["omniauth.auth"]
