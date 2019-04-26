196feb7d2f8b31384a8e205be0604dc41ac945f7
Ricardo <ry.yrupailla@gmail.com>
2019-04-26 12:40:41 -0500

    Add .env example

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   
-  root 'welcome#index'
+  root 'home#index'
 
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   # resources  :users, :only => [:index, :show]
