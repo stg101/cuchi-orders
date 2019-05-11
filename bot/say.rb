a0ad7a68cd12974c17bef79d857bd0d14c779cbb
Santiago <ry.yrupailla@gmail.com>
2019-05-11 17:37:52 -0500

    Add test commands

--- bot/say.rb
+++ bot/say.rb
@@ -39,4 +39,17 @@ class Say < SlackRubyBot::Commands::Base
     message[:text] = ''
     client.web_client.chat_postMessage(message)
   end
+
+  command 'que deseas?' do |client, data, match|
+    message = {}
+    message[:channel] = data.channel
+    message[:as_user] = true
+    message[:text] = 'proveer comida es mi pasion'
+    client.web_client.chat_postMessage(message)
+  end
+
+  command 'ping' do |client, data, match|
+    client.say(text: 'pong', channel: data.channel)
+  end
+
 end
